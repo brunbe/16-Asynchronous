3922f8225ce1b9fd538c421f114aefb7ed3462b9
bruno de clercq <brunojl.declercq@gmail.com>
2023-05-26 08:43:47 +0200

    ajax

--- starter/script.js
+++ starter/script.js
@@ -7,7 +7,30 @@ const countriesContainer = document.querySelector('.countries');
 
 //THE OLD SCHOOL WAY OF DOING AJAX: XMLHttpRequest-function
 
-const getCountryData = function (country) {
+const renderCountry = function (data, className = ' ') {
+  const html = `
+<article class="country ${className}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}M people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages
+      .map(el => el.name)
+      .join(', ')}</p>
+    <p class="country__row"><span>💰</span>${data.currencies
+      .map(el => el.name)
+      .join(', ')}</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = '1';
+};
+//AJAX call country 1;
+const getCountryAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
   request.send();
@@ -16,31 +39,27 @@ const getCountryData = function (country) {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
 
-    const html = `
-        <article class="country">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)}M people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages
-              .map(el => el.name)
-              .join(', ')}</p>
-            <p class="country__row"><span>💰</span>${data.currencies
-              .map(el => el.name)
-              .join(', ')}</p>
-          </div>
-        </article>`;
+    //render country 1;
+    renderCountry(data);
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = '1';
+    //Get neighbour country (2)
+    const neighbour = data.borders?.[0];
+    if (!neighbour) return;
+    //AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      //render country 2;
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
 // getCountryData('belgium');
-getCountryData('portugal');
+getCountryAndNeighbour('portugal');
 // getCountryData('france');
 // getCountryData('spain');
 // getCountryData('canada');
